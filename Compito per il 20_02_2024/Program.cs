5da663d6d9c2b7c05d9732a8647e4bdeeceae3da
Adam Ben jeddou <mcadambenjeddou@gmail.com>
2024-02-21 00:39:05 +0100

    Aggiungere i file di progetto.

--- Compito per il 20_02_2024/Program.cs
+++ Compito per il 20_02_2024/Program.cs
@@ -18,9 +18,11 @@ namespace Compito_per_il_20_02_2024
             Corpo m3 = new Corpo(m2.Massa, new Vettore(400e6, 400e6));
             Corpo m4 = new Corpo(m2.Massa, new Vettore(0, 400e6));
 
-            Vettore gravità2_1 = G * m1.Massa * m2.Massa / Vettore.Modulo(m2.Posizione) * (m2.Posizione / Vettore.Modulo(m2.Posizione));
-            Vettore gravità3_1 = G * m1.Massa * m3.Massa / Vettore.Modulo(m3.Posizione) * (m3.Posizione / Vettore.Modulo(m3.Posizione));
-            Vettore gravità4_1 = G * m1.Massa * m4.Massa / Vettore.Modulo(m4.Posizione) * (m4.Posizione / Vettore.Modulo(m4.Posizione));
+            Vettore gravità2_1 = G * m1.Massa * m2.Massa / (Vettore.Modulo(m2.Posizione) * Vettore.Modulo(m2.Posizione)) * (m2.Posizione / Vettore.Modulo(m2.Posizione));
+            Vettore gravità3_1 = G * m1.Massa * m3.Massa / (Vettore.Modulo(m3.Posizione) * Vettore.Modulo(m3.Posizione)) * (m3.Posizione / Vettore.Modulo(m3.Posizione));
+            Vettore gravità4_1 = G * m1.Massa * m4.Massa / (Vettore.Modulo(m4.Posizione) * Vettore.Modulo(m4.Posizione)) * (m4.Posizione / Vettore.Modulo(m4.Posizione));
+
+            Vettore risultante = gravità2_1 + gravità3_1 + gravità4_1;
 
             Console.ReadKey();
         }
